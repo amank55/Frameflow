53c9a770331054c73f29e4ab64c0f68b9186f2ce
amank55 <amankumarr2005ak@gmail.com>
2025-01-23 15:43:32 +0530

    Add workspace selection to Sidebar component with routing functionality

--- framesapp/src/components/global/sidebar/index.tsx
+++ framesapp/src/components/global/sidebar/index.tsx
@@ -1,4 +1,7 @@
+"use client"
+import { Select, SelectContent, SelectTrigger, SelectValue } from '@radix-ui/react-select'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 type Props = {
@@ -6,8 +9,12 @@ type Props = {
 }
 
 const Sidebar = ({activeWorkspaceId}: Props) => {
+  const router = useRouter()
+  const onChangeActiveWorkSpace = (value: string) => {
+    router.push(`/dashboard/${value}`)
+  }
   return (
-    <div className='bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center overflow-hidden'>
+    <div className='bg-[#111111] flex-none relative p-3 h-full w-[170px] flex flex-col gap-4 items-center overflow-hidden'>
      <div className='bg-[#111111] justify-center items-center p-4 gap-2 mb-4 absolute top-0 left-0 right-0'>
       <Image
       src= "/frameflow.png"
@@ -15,7 +22,13 @@ const Sidebar = ({activeWorkspaceId}: Props) => {
       height = {40}
       width = {40}
       />
-      <p className='text-2xl'>Frameflow</p>
+      <p className='text-1xl font-extrabold'>frameflow</p>
+      <Select defaultValue={activeWorkspaceId} onValueChange={ onChangeActiveWorkSpace}>
+      <SelectTrigger className="mt-2 text-neutral-300 bg-transparent">
+          <SelectValue placeholder="Select a workspace"></SelectValue>
+        </SelectTrigger>
+        <SelectContent ></SelectContent>
+      </Select>
      </div>
     </div>
   )
