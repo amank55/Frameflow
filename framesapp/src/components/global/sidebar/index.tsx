c8eb2faf912ccd9a46daffbd9e5f3d40b948b73d
amank55 <amankumarr2005ak@gmail.com>
2025-01-23 14:23:22 +0530

    Enhance Sidebar component: add logo and title with styling improvements

--- framesapp/src/components/global/sidebar/index.tsx
+++ framesapp/src/components/global/sidebar/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 type Props = {
@@ -6,7 +7,17 @@ type Props = {
 
 const Sidebar = ({activeWorkspaceId}: Props) => {
   return (
-    <div>Slider</div>
+    <div className='bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center overflow-hidden'>
+     <div className='bg-[#111111] justify-center items-center p-4 gap-2 mb-4 absolute top-0 left-0 right-0'>
+      <Image
+      src= "/frameflow.png"
+      alt = "logo"
+      height = {40}
+      width = {40}
+      />
+      <p className='text-2xl'>Frameflow</p>
+     </div>
+    </div>
   )
 }
 
