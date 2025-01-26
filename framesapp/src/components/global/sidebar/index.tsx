92ac7a5d9c5ff67d5a9b68d437a4b1de0be45abf
amank55 <amankumarr2005ak@gmail.com>
2025-01-26 12:32:35 +0530

    Update package dependencies and refactor Sidebar component: upgrade React and React DOM versions, remove unused hooks and types, and clean up import paths.

--- framesapp/src/components/global/sidebar/index.tsx
+++ framesapp/src/components/global/sidebar/index.tsx
@@ -1,42 +1,29 @@
 "use client";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
+import { Select, SelectContent, SelectTrigger, SelectValue, SelectItem, SelectGroup, SelectLabel } from '@radix-ui/react-select';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import React from 'react';
-import { Separator } from '@/components/ui/separator';
-import { useQueryData } from '@/hooks/useQueryData';
-import { WorkspaceProps } from '@/types/index.type';
-import { workAsyncStorage } from 'next/dist/server/app-render/entry-base';
+//import { useQueryData } from '@/hooks/useQueryData';
 import { getWorkSpaces } from '@/actions/workspace';
-
+//import { WorkspaceProps } from '@/types/index.type';
+import { Separator } from '@/components/ui/separator';
 
 type Props = {
   activeWorkspaceId: string;
 };
 
-const { data, isFetched } = useQueryData(['user-workspaces'], getWorkSpaces)
-const { data: workspace } = data as WorkspaceProps
-
 const Sidebar = ({ activeWorkspaceId }: Props) => {
   const router = useRouter();
+
+ 
+
   const onChangeActiveWorkSpace = (value: string) => {
     router.push(`/dashboard/${value}`);
   };
 
-  
-
-
   return (
-    <div className='bg-[#111111] flex-none relative p-3 h-full w-[180px] flex flex-col gap-4 items-center overflow-hidden'>
-      <div className='bg-[#111111] justify-center items-center p-4 gap-2 mb-4 absolute top-0 left--1 right-0'>
+    <div className='bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col gap-4 items-center overflow-hidden'>
+      <div className='bg-[#111111] justify-center items-center p-4 gap-2 mb-4 absolute top-0 left-0 right-0'>
         <Image
           src="/frameflow.png"
           alt="logo"
@@ -48,13 +35,6 @@ const Sidebar = ({ activeWorkspaceId }: Props) => {
           <SelectTrigger className="mt-2 text-neutral-300 bg-transparent">
             <SelectValue placeholder="Select a workspace" />
           </SelectTrigger>
-          <SelectContent className='bg-[#111111] backdrop-blur-xl'>
-            <SelectGroup>
-              <SelectLabel>Workspaces</SelectLabel>
-             <Separator/>
-             
-            </SelectGroup>
-          </SelectContent>
         </Select>
       </div>
     </div>
